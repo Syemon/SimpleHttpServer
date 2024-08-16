72853d71906e24e0e0c1f483677e57cb5d8020cd
Szymon Linowski <sz.linowski@gmail.com>
2024-08-16 14:03:17 +0200

    Implement simple wrapper of HttpServer

--- src/main/java/org/syemon/SimpleHttpServer.java
+++ src/main/java/org/syemon/SimpleHttpServer.java
@@ -2,6 +2,10 @@ package org.syemon;
 
 import com.sun.net.httpserver.HttpServer;
 
+import java.time.Duration;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class SimpleHttpServer {
     private final HttpServer httpServer;
 
@@ -10,11 +14,14 @@ public class SimpleHttpServer {
     }
 
     public void start() {
-        long startTime =  System.currentTimeMillis();
-        System.out.println("Server start at: " + startTime);
+        OffsetDateTime serverStartTime = OffsetDateTime.now();
+
+        System.out.println("Server start at: " + serverStartTime.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME));        //print serverStartTime variable to console in iso format (with miliseconds)
+
         httpServer.start();
-        long endTime = System.currentTimeMillis();
-        System.out.println("Server executed at: " + System.currentTimeMillis() + ". It took " + (endTime - startTime) + " miliseconds");
+        OffsetDateTime serverEndTime = OffsetDateTime.now();
+        long durationInMillis = Duration.between(serverStartTime, serverEndTime).toMillis();
+        System.out.printf("Server executed at: %s. It took %d milliseconds", serverEndTime.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME), durationInMillis);
 
     }
 
--- src/test/java/org/syemon/SimpleHttpServerTest.java
+++ src/test/java/org/syemon/SimpleHttpServerTest.java
@@ -1,19 +1,13 @@
 package org.syemon;
 
-import lombok.extern.java.Log;
-import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URI;
-import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -24,13 +18,15 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
 public class SimpleHttpServerTest {
 
-    public static final int CONNECT_TIMEOUT = 5000;
-    public static final int READ_TIMEOUT = 5000;
+    public static final String HOST = "localhost";
+    public static final int PORT = 8080;
+    public static final int THREAD_POOL_SIZE = 10;
+    private static final String URL = "http://%s:%s/multi".formatted(HOST, PORT);
+
     private final HttpServerFactory httpServerFactory = new HttpServerFactory();
 
     private SimpleHttpServer simpleHttpServer;
@@ -42,15 +38,14 @@ public class SimpleHttpServerTest {
     public void setUp() throws IOException {
         ConcurrentCollectionHttpHandler.CONCURRENT_MAP.clear();
 
-
         HttpServerConfig httpServerConfig = new HttpServerConfig()
-                .setHost("localhost")
-                .setPort(8080)
-                .setThreadPoolSize(10);
+                .setHost(HOST)
+                .setPort(PORT)
+                .setThreadPoolSize(THREAD_POOL_SIZE);
         simpleHttpServer = new SimpleHttpServer(httpServerFactory.create(httpServerConfig));
         simpleHttpServer.start();
         client = HttpClient.newHttpClient();
-        URI uri = URI.create("http://localhost:8080/multi");
+        URI uri = URI.create(URL);
         request = HttpRequest.newBuilder()
                 .uri(uri)
                 .GET()
