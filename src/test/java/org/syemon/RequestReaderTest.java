217701193ecf15b8e464279095615bcf6b5230b2
Szymon Linowski <sz.linowski@gmail.com>
2024-08-25 12:23:26 +0200

    Add unit test

--- src/test/java/org/syemon/RequestReaderTest.java
+++ src/test/java/org/syemon/RequestReaderTest.java
@@ -1,10 +1,13 @@
 package org.syemon;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,17 +15,30 @@ class RequestReaderTest {
 
     RequestReader sut = new RequestReader();
 
-    @Test
-    void decode() {
+    @ParameterizedTest
+    @MethodSource("decodeDataProvider")
+    void decode(HttpMethod httpMethod, String path) {
         // given
-        InputStream inputStream = new ByteArrayInputStream("GET / HTTP/1.1\r\n".getBytes());
+        String request = "%s %s HTTP/1.1\r\n".formatted(httpMethod, path);
+
+        InputStream inputStream = new ByteArrayInputStream(request.getBytes());
 
         // when
         Optional<HttpRequest> actual = sut.decode(inputStream);
 
         // then
         assertThat(actual).isPresent();
-        assertThat(actual.get().getMethod()).isEqualTo(HttpMethod.GET);
-        assertThat(actual.get().getPath()).isEqualTo("/");
+        assertThat(actual.get().getMethod()).isEqualTo(httpMethod);
+        assertThat(actual.get().getPath()).isEqualTo(path);
     }
+
+    public static Stream<Arguments> decodeDataProvider() {
+        return Stream.of(
+                Arguments.of(HttpMethod.GET, "/loremipsum1"),
+                Arguments.of(HttpMethod.POST, "/loremipsum2"),
+                Arguments.of(HttpMethod.PUT, "/loremipsum3"),
+                Arguments.of(HttpMethod.DELETE, "/loremipsum4")
+        );
+    }
+
 }
